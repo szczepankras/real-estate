765c4dfffb00ad931efc32a6b37eca18ca020376
szczepankras <szczepankras@gmail.com>
2016-05-21 14:05:08 +0200

    Angular and babel configured

--- content/index.js
+++ content/index.js
@@ -2,4 +2,6 @@
  * @author Szczepan
  * Created on 21.05.2016.
  */
-document.write("It works.");
+import angular from 'angular';
+const ngModule = angular.module('app', []);
+console.log(ngModule);
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     module: {
         loaders: [
+            {test: /\.js$/, loader: 'babel', exclude: /node_modules/},
             { test: /\.css$/, loader: "style!css" }
         ]
     }
