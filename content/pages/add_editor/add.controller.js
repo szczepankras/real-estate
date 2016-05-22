1c24740bc6f623f5a56111c886b3941e27e287ce
szczepankras <szczepankras@gmail.com>
2016-05-22 23:39:23 +0200

    Add to database script created

--- content/pages/add_editor/add.controller.js
+++ content/pages/add_editor/add.controller.js
@@ -3,11 +3,28 @@
  * Created on 21.05.2016.
  */
 export default class AddController {
-    constructor(SetAdvertisementsService) {
+    constructor(SetAdvertisementsService, $scope) {
+        this.$scope = $scope;
         this.setAdvertisementService = SetAdvertisementsService;
     }
 
-    saveRecord(){
-        this.setAdvertisementService.setAdvertisement({"content": "dupa", "date": "20-05-2016"});
+    addRecord() {
+        var record = {
+            "title": this.$scope.title,
+            "content": this.$scope.description,
+            "number": this.$scope.number
+        };
+        this.setAdvertisementService.setAdvertisement(record);
+        this.clearInputs();
+    }
+
+    clearInputs() {
+        this.$scope.title = "";
+        this.$scope.description = "";
+        this.$scope.number = "";
+    }
+
+    onItemClick(option){
+        console.log(option);
     }
 }
